1b6620116c22e81615d41ba4c87df2a3ce1d9037
Nihal Adhikari <nihaladhikari998@hmail.com>
2026-05-31 21:11:10 +0530

    Changes About Section

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-about',
@@ -8,5 +12,63 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  roles = [
+    'Angular Developer',
+    'Frontend Developer',
+    'UI Enthusiast'
+  ];
+
+  currentRole = '';
+  private roleIndex = 0;
+  private charIndex = 0;
+  private isDeleting = false;
+
+  ngOnInit(): void {
+    this.typeEffect();
+  }
+
+  typeEffect(): void {
+    const currentText =
+      this.roles[this.roleIndex];
+
+    if (!this.isDeleting) {
+      this.currentRole = currentText.substring(0, this.charIndex + 1);
+
+      this.charIndex++;
+
+      if (this.charIndex === currentText.length) {
+        this.isDeleting = true;
+        setTimeout(() => this.typeEffect(), 1500);
+        return;
+      }
+    } else {
+      this.currentRole = currentText.substring(0, this.charIndex - 1);
+
+      this.charIndex--;
+      if (this.charIndex === 0) {
+        this.isDeleting = false;
+        this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+      }
+    }
+    setTimeout(
+      () => this.typeEffect(),
+      this.isDeleting ? 50 : 100
+    );
+  }
+
+  ngAfterViewInit() {
+    gsap.from('.about-reveal', {
+      y: 80,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.15,
+
+      scrollTrigger: {
+        trigger: '#about',
+        start: 'top 80%',
+      },
+    });
+  }
 
 }
